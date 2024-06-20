22f24f542c49984516e8087d3aaf7f95daa40249
yanisa <75199035+bowyani@users.noreply.github.com>
2024-06-20 12:29:28 +0700

    added app.get('/', (req, res) => {     res.render('Views/Home') }) tag

--- Index.js
+++ Index.js
@@ -10,7 +10,7 @@ app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname, 'Public')));
 
 app.get('/', (req, res) => {
-    res.render('Home')
+    res.render('Views/Home')
 })
 
 app.listen(port, () => {
