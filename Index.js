6281e86b83ea5b414744a9d6d65ef61fed0af775
yanisa <75199035+bowyani@users.noreply.github.com>
2024-06-20 12:24:22 +0700

    added </script> tag

--- Index.js
+++ Index.js
@@ -4,10 +4,10 @@ var path = require('path');
 
 let port = process.env.PORT || 3000;
 
-app.set('views', path.join(__dirname,'views'));
+app.set('views', path.join(__dirname,'Views'));
 app.set('view engine', 'ejs');
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'Public')));
 
 app.get('/', (req, res) => {
     res.render('Home')
